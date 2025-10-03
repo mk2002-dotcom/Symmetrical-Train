2480abde6091587675332b8180ad35a1792345c2
mk2002-dotcom <nari110114@gmail.com>
2025-10-03 15:14:57 +0900

    Update and rename compress.py to ImageCompression/compress.py

--- ImageCompression/compress.py
+++ ImageCompression/compress.py
@@ -1,21 +1,23 @@
+# compression with neural network
 import torch
 import matplotlib.pyplot as plt
 from PIL import Image
 from compressai.zoo import bmshj2018_factorized
 from torchvision import transforms
 
-# 学習済みモデルの読み込み
+# loading a trained model
 net = bmshj2018_factorized(quality=1, pretrained=True).eval()
 
 img = Image.open('images.jpg').convert('L')
 img = img.convert('RGB')
 x = transforms.ToTensor()(img).unsqueeze(0)
 
-# 圧縮
+# compression
 out = net.compress(x)
-strings = out["strings"]  # ビットストリーム
+strings = out["strings"]
 shape = out["shape"]
 
-# 復元
+# restoration
 x_hat = net.decompress(strings, shape)["x_hat"].clamp(0, 1)
+
 Image.fromarray((x_hat.squeeze().permute(1, 2, 0).detach().numpy()*255).astype("uint8")).save("output.png")
